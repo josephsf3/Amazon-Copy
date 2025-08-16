e06fccfba6c58bc11a249a761a4257305bb834fe
Joseph <josepch.1057@gmail.com>
2025-08-16 13:25:45 +0530

    load producst from backend

--- data/products.js
+++ data/products.js
@@ -22,7 +22,7 @@ class Product {
     this.image = productDetails.image;
     this.name =  productDetails.name;
     this.rating = productDetails.rating;
-    this.price = productDetails.price;
+    this.price = productDetails.priceCents;
   }
 
   getStarsUrl () {
@@ -71,6 +71,30 @@ class Appliances extends Product{
 
 }
 
+export let products = [];
+
+export function loadProducts (fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      if (productDetails.type === 'appliance') {
+        return new Appliances(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    fun();
+  })
+  
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -751,3 +775,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,88 +1,94 @@
 import {cart, addToCart, updateCartQuantity} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 //Modules help us with naming conflicts and we dont have to worry about order of our files or script tags
-let productsHTML = '';
 
-products.forEach(product => {
-    productsHTML += `
-        <div class="product-container">
-            <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
-            </div>
+loadProducts(renderProductsGrid);
 
-            <div class="product-name limit-text-to-2-lines">
-                ${product.name}
-            </div>
+function renderProductsGrid () {
+    let productsHTML = '';
 
-            <div class="product-rating-container">
-                <img class="product-rating-stars"
-                src="${product.getStarsUrl()}">
-                <div class="product-rating-count link-primary">
-                ${product.rating.count}
+    products.forEach(product => {
+        productsHTML += `
+            <div class="product-container">
+                <div class="product-image-container">
+                    <img class="product-image"
+                    src="${product.image}">
                 </div>
-            </div>
 
-            <div class="product-price">
-                ${product.getPrice()}
-            </div>
+                <div class="product-name limit-text-to-2-lines">
+                    ${product.name}
+                </div>
 
-            <div class="product-quantity-container">
-                <select class = "js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                </select>
-            </div>
-            ${product.extraInfoHTML()}
-            <div class="product-spacer"></div>
+                <div class="product-rating-container">
+                    <img class="product-rating-stars"
+                    src="${product.getStarsUrl()}">
+                    <div class="product-rating-count link-primary">
+                    ${product.rating.count}
+                    </div>
+                </div>
 
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-                <img src="images/icons/checkmark.png">
-                Added
-            </div>
+                <div class="product-price">
+                    ${product.getPrice()}
+                </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
-                Add to Cart
-            </button>
-            </div>
-    `;
-});
+                <div class="product-quantity-container">
+                    <select class = "js-quantity-selector-${product.id}">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    </select>
+                </div>
+                ${product.extraInfoHTML()}
+                <div class="product-spacer"></div>
 
-let timeouts = {};
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+                <div class="added-to-cart js-added-to-cart-${product.id}">
+                    <img src="images/icons/checkmark.png">
+                    Added
+                </div>
 
-updateCartQuantityHome();
-
-function updateCartQuantityHome() {
-    document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity();
-}
-
-function addedToCartDisplay(productId) {
-    document.querySelector(`.js-added-to-cart-${productId}`).classList.add('css-added-to-cart-visible');
-
-    if (timeouts[productId]) {
-        clearTimeout(timeouts[productId]);
-    }
-    timeouts[productId] = setTimeout(() => {
-        document.querySelector(`.js-added-to-cart-${productId}`).classList.remove('css-added-to-cart-visible');
-    }, 2000);
-}
-
-
-document.querySelectorAll('.js-add-to-cart-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const productId = button.dataset.productId;
-        addToCart(productId);
-        updateCartQuantityHome();
-        addedToCartDisplay(productId);   
+                <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
+                    Add to Cart
+                </button>
+                </div>
+        `;
     });
-});
+
+    let timeouts = {};
+    document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+    updateCartQuantityHome();
+
+    function updateCartQuantityHome() {
+        document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity();
+    }
+
+    function addedToCartDisplay(productId) {
+        document.querySelector(`.js-added-to-cart-${productId}`).classList.add('css-added-to-cart-visible');
+
+        if (timeouts[productId]) {
+            clearTimeout(timeouts[productId]);
+        }
+        timeouts[productId] = setTimeout(() => {
+            document.querySelector(`.js-added-to-cart-${productId}`).classList.remove('css-added-to-cart-visible');
+        }, 2000);
+    }
+
+
+    document.querySelectorAll('.js-add-to-cart-button').forEach(button => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantityHome();
+            addedToCartDisplay(productId);   
+        });
+    });
+
+}
