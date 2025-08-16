252ca77ba86ca1898b3d9c7d3b541fec083c0975
Joseph <josepch.1057@gmail.com>
2025-08-16 12:56:31 +0530

    Add backend practise file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-oops.js';
 //import '../data/cart-class.js';
+import '../data/backend-practise.js';
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
