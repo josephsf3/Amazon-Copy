68e7f3a77c2487afa37f0db335bbdb6b14561435
Joseph <josepch.1057@gmail.com>
2025-09-10 16:33:41 +0530

    env variable

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = 'mongodb+srv://Amazon:Amazon@josephscluster.esvg4gs.mongodb.net/Amazon';
+const MONGO_URI = process.env.MONGO_URI;
 
 export async function connectDB() {
   try {
